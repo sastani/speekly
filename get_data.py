6f445e7e3574f06ba0a2c88da4b1498ea88dfbca
Tom O'Connell <toconnel@caltech.edu>
2017-03-04 01:24:21 -0800

    Removed unused code from get_data

--- get_data.py
+++ get_data.py
@@ -5,8 +5,8 @@ import librosa
 from pytube import YouTube
 import numpy as np
 import pandas as pd
+from ntlk.tokenize import word_tokenize
 
-'''
 def parse_time_str(s):
     """
     Takes HH:MM:SS to seconds in floats.
@@ -54,76 +54,3 @@ def mp4_to_raw(mp4):
 
     # wait for it to finish? default?
     sp.Popen(['ffmpeg', '-i', mp4, '-f-', 's161e', '-acodec', 'pcm_s16le', outname])
-    # TODO 
-    df.loc[video == mp4, 'audio_file'] = outname
-'''
-
-
-def words(text):
-    # TODO clean off punctuation. restrict to some set of characters?
-    return text.split()
-
-
-def phonemes(text):
-    assert False
-
-
-def sentences(text):
-    punctuation = {} # TODO
-
-    # split on set?
-
-    return text.split(punctuation)
-
-def normalize_text(text):
-
-
-# TODO general function to clean / normalize text?
-
-
-# in case we want to find videos of kids speaking known texts
-# and see how our pipeline works on their audio
-"""
-label_csv = 'ht17_links.csv'
-input_df = pd.read_csv(label_csv)
-
-'''
-input_df['start'] = input_df['start'].map(lambda s: parse_time_str(s))
-input_df['end'] = input_df['end'].map(lambda s: parse_time_str(s))
-'''
-
-df_pickle = 'ht17.p'
-if not os.file.exists(df_pickle):
-    # make a new df that also has columns for video file name and audio file name
-    # TODO pickle this dataframe and load at beginning as well
-    df = input_df.concat({'video_file': ,'audio_file': })
-
-else:
-    # how to load pickled df?
-
-# go through all links
-# adds video_file names to df once they download
-get_links(df)
-
-for i in range(len(df['link'])):
-    mp4_to_raw(df['link'][i])
-
-
-"""
-
-template = ''
-
-# TODO by phoneme / word / sentence ? depends on API
-# robust ways to split on each of these?
-
-# upload these raw audio files in chunks to the speech-to-text API
-
-# receive predicted text
-
-# see if predicted text matches template
-
-# if it doesn't, output audio corresponding to correct pronunciation
-# use text-to-speech API
-
-
-# pickle df if using it
